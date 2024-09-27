b567e1976c42d8e7081129e3f816be6fc05fd2fc
Adrian Schiopu <adrian.schiopu19@gmail.com>
2024-09-27 16:15:17 +0300

    Preview added

--- app/src/main/java/com/example/random/MainActivity.kt
+++ app/src/main/java/com/example/random/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import kotlinx.coroutines.launch
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -137,4 +138,10 @@ fun RandomGenerator() {
 
 fun generateRandomNumber(min: Int, max: Int): Int {
     return (min..max).random()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewRandomGenerator() {
+    RandomGenerator()
 }
